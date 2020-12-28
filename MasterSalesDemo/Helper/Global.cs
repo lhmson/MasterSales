5631c7c3bf3191ed7a7e09bbdba5ae7cbf7d42b0
Pham Lien Sanh <18520146@gm.uit.edu.vn>
2020-12-29 00:07:14 +0700

    Chuyen chuc vu

---
+++ MasterSalesDemo/Helper/Global.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MasterSalesDemo.Model;
+using System.Collections.ObjectModel;
+using System.Windows.Forms;
+
+namespace MasterSalesDemo.Helper
+{
+    public class Global
+    {
+        public NHANVIEN NhanVien { get; set; }
+        public string NgayThangNam { get; set; }
+
+        public bool isValid { get; set; } 
+
+        private Global ()
+        {
+            isValid = false;
+        }
+
+        //Hàm Generate Mã tự động
+        public int filterNumber(string code)
+        {
+            string number = "";
+            for (int i = 0; i < code.Length; i++)
+                if (code[i] >= '0' && code[i] <= '9')
+                    number += code[i];
+            return int.Parse(number);
+        }
+
+        //ex: auto Generate("LS", 123, 8) => code : LS000123
+        public string autoGenerateCode(string quydinh, int max, int length)
+        {
+            int length_numbers0 = length - quydinh.Length - (max + "").Length;
+            string code = quydinh;
+            for (int i = 1; i <= length_numbers0; i++)
+                code += "0";
+            code += max + "";
+            return code;
+        }
+
+        public string autoGenerateLichSu()
+        {
+            //Loại bỏ chữ cái ở trước
+            int flag = 0;
+            ObservableCollection<LICHSUCHUCVU> _listLS = new ObservableCollection<LICHSUCHUCVU>(DataProvider.Ins.DB.LICHSUCHUCVUs);
+            foreach (var ls in _listLS)
+            {
+                int number = filterNumber(ls.id);
+                if (number > flag)
+                    flag = number;
+            }
+
+            flag++;
+            return autoGenerateCode("LS", flag, 7);
+        }
+
+        public void setNhanVien(NHANVIEN nv)
+        {
+            this.NhanVien = nv;
+            NgayThangNam = DateTime.Now.ToString("dd/MM/yyyy");
+        }
+
+        private static Global _instance = null;
+        public static Global Ins
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Global();
+                }
+                return _instance;
+            }
+
+        }
+
+        public TAIKHOAN TaoChucVuNhanVien(NHANVIEN nhanvien, CHUCVU chucvu)
+        {
+            TAIKHOAN taikhoan = getTaiKhoanbyMaNV(nhanvien.id);
+
+            //Nếu chưa có tài khoản, chưa làm
+
+            //Nếu có tài khoản, đổi chức vụ mới
+            taikhoan.MaChucVu = chucvu.id;
+            DataProvider.Ins.DB.SaveChanges();
+
+            //Tạo trong lịch sử
+            LICHSUCHUCVU lichsu = new LICHSUCHUCVU()
+            {
+                id = autoGenerateLichSu(),
+                MaNV = nhanvien.id,
+                MaChucVu = chucvu.id,
+                NgayBD = DateTime.Now,
+                NgayKT = DateTime.Now.AddDays(1),
+                isDeleted = false,
+            };
+
+            DataProvider.Ins.DB.LICHSUCHUCVUs.Add(lichsu);
+            updateLichSu(nhanvien);
+            DataProvider.Ins.DB.SaveChanges();
+            return taikhoan;
+        }
+        public CHUCVU getChucVubyMaNV(string MaNV)
+        {
+            ObservableCollection<TAIKHOAN> _listTaiKhoan = new ObservableCollection<TAIKHOAN>(DataProvider.Ins.DB.TAIKHOANs);
+
+            foreach (var ls in _listTaiKhoan)
+            {
+                if (ls.MaNV == MaNV) 
+                {
+                    return ls.CHUCVU;
+                }                    
+            }
+            return null;
+        }
+        public CHUCVU getChucVubyTenCVTenPB(string TenCV, string TenPB)
+        {
+            ObservableCollection<CHUCVU> _listChucVu = new ObservableCollection<CHUCVU>(DataProvider.Ins.DB.CHUCVUs);
+
+            foreach (var cv in _listChucVu)
+                if (cv.TenChucVu == TenCV && cv.PHONGBAN.TenPhong == TenPB)
+                    return cv;
+
+            return null;
+        }
+        public NHANVIEN getNhanVienbyMaNV(string MaNV)
+        {
+            ObservableCollection<NHANVIEN> _listNhanVien = new ObservableCollection<NHANVIEN>(DataProvider.Ins.DB.NHANVIENs);
+
+            foreach (var nv in _listNhanVien)
+            {
+                if (nv.id == MaNV)
+                    return nv;
+            }
+
+            return null;
+        }
+
+        public HOPDONG getHopDongbyMaNV(string MaNV)
+        {
+            ObservableCollection<HOPDONG> _listHopDong = new ObservableCollection<HOPDONG>(DataProvider.Ins.DB.HOPDONGs);
+
+            foreach (var hopdong in _listHopDong)
+                if (hopdong.MaNV == MaNV && hopdong.isDeleted == false)
+                    return hopdong;
+            return null;
+        }
+
+        public PHONGBAN getPhongBanbyMaPB(string MaPB)
+        {
+            ObservableCollection<PHONGBAN> _listPhongBan = new ObservableCollection<PHONGBAN>(DataProvider.Ins.DB.PHONGBANs);
+            foreach (var pb in _listPhongBan)
+                if (pb.id == MaPB)
+                    return pb;
+            return null;
+        }
+
+        public PHONGBAN getPhongBanbyTenPB(string TenPB)
+        {
+            ObservableCollection<PHONGBAN> _listPhongBan = new ObservableCollection<PHONGBAN>(DataProvider.Ins.DB.PHONGBANs);
+            foreach (var pb in _listPhongBan)
+                if (pb.TenPhong == TenPB)
+                    return pb;
+            return null;
+        }
+        
+        public TAIKHOAN getTaiKhoanbyMaNV(string MaNV)
+        {
+            ObservableCollection<TAIKHOAN> _listTaiKhoan = new ObservableCollection<TAIKHOAN>(DataProvider.Ins.DB.TAIKHOANs);
+            foreach (var tk in _listTaiKhoan)
+                if (tk.MaNV == MaNV)
+                    return tk;
+            return null;
+        }
+        //Functions load database
+        #region
+        public ObservableCollection<string> getAllTenPhongBan()
+        {
+            ObservableCollection<string>  ListPhongBan = new ObservableCollection<string>();
+            ObservableCollection<PHONGBAN> _listPhongBan = new ObservableCollection<PHONGBAN>(DataProvider.Ins.DB.PHONGBANs);
+            foreach (var pb in _listPhongBan)
+                ListPhongBan.Add(pb.TenPhong);
+            return ListPhongBan;
+        }
+
+        public ObservableCollection<string> getAllTenChucVubyMaPB(string MaPhongBan)
+        {
+            ObservableCollection<string>  ListChucVu = new ObservableCollection<string>();
+            ObservableCollection<CHUCVU> _listChucVu = new ObservableCollection<CHUCVU>(DataProvider.Ins.DB.CHUCVUs);
+            foreach (var cv in _listChucVu)
+                if (cv.PHONGBAN.id == MaPhongBan)
+                    ListChucVu.Add(cv.TenChucVu);
+            return ListChucVu;
+        }
+        #endregion
+
+        //Functions sub
+        public void updateLichSu(NHANVIEN nhanvien)
+        {
+            ObservableCollection<LICHSUCHUCVU> _listLS = new ObservableCollection<LICHSUCHUCVU>(DataProvider.Ins.DB.LICHSUCHUCVUs);
+
+            foreach (var ls in _listLS)
+                if (ls.MaNV == nhanvien.id)
+                    if (ls.NgayBD.AddDays(1) > ls.NgayKT)
+                        ls.NgayKT = DateTime.Now;
+        }
+    }
+}
--- MasterSalesDemo/ViewModel/QLLichSu_ViewModel.cs
+++ MasterSalesDemo/ViewModel/QLLichSu_ViewModel.cs
@@ -13,13 +13,242 @@ using System.Windows;
 using System.Windows.Documents;
 using MasterSalesDemo.View;
 using System.Windows.Input;
-using System.Runtime.InteropServices;
-using Excel = Microsoft.Office.Interop.Excel;
+using MasterSalesDemo.Helper;
+
+
 
 namespace MasterSalesDemo.ViewModel
 {
+    public class ThongTinNhanVien
+    {
+        public string STT { get; set; }
+        public string MaNV { get; set; }
+        public string HoTen { get; set; }
+        public string ChucVu { get; set; }
+        public string PhongBan { get; set; }
+
+        public ThongTinNhanVien(int stt, string MaNV, string HoTen, string PhongBan, string ChucVu)
+        {
+            this.STT = stt + "";
+            this.MaNV = MaNV;
+            this.HoTen = HoTen;
+            this.PhongBan = PhongBan;
+            this.ChucVu = ChucVu;
+        }
+    }
+
+    public class QuaTrinhLamViec
+    {
+        public string STT { get; set; }
+        public string ChucVu { get; set; }
+        public string PhongBan { get; set; }
+        public string NgayBatDau { get; set; }
+        public string NgayKetThuc { get; set; }
+
+        public QuaTrinhLamViec(int stt, string ChucVu, string PhongBan, string NgayBatDau, string NgayKetThuc)
+        {
+            this.STT = stt + "";
+            this.ChucVu = ChucVu;
+            this.PhongBan = PhongBan;
+            this.NgayBatDau = NgayBatDau;
+            this.NgayKetThuc = NgayKetThuc;
+        }
+    }
+
     public class QLLichSu_ViewModel : BaseViewModel
     {
+        #region Binding Variables
+        private ObservableCollection<string> _ListPhongBan;
+        public ObservableCollection<string> ListPhongBan
+        {
+            get { return _ListPhongBan; }
+            set { _ListPhongBan = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<ThongTinNhanVien> _ListThongTinNhanVien;
+        public ObservableCollection<ThongTinNhanVien> ListThongTinNhanVien
+        {
+            get { return _ListThongTinNhanVien; }
+            set { _ListThongTinNhanVien = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<QuaTrinhLamViec> _ListQuaTrinhNhanVien;
+        public ObservableCollection<QuaTrinhLamViec> ListQuaTrinhNhanVien
+        {
+            get { return _ListQuaTrinhNhanVien; }
+            set { _ListQuaTrinhNhanVien = value; OnPropertyChanged(); }
+        }
+
+        private string _SelectedPhongBan;
+        public string SelectedPhongBan
+        {
+            get { return _SelectedPhongBan; }
+            set { _SelectedPhongBan = value; OnPropertyChanged(); }
+        }
+
+        private string _ContentCommand;
+        public string ContentCommand
+        {
+            get { return _ContentCommand; }
+            set { _ContentCommand = value; OnPropertyChanged(); }
+        }
+
+        private ThongTinNhanVien _SelectedNhanVien;
+        public ThongTinNhanVien SelectedNhanVien
+        {
+            get { return _SelectedNhanVien; }
+            set { _SelectedNhanVien = value; OnPropertyChanged(); }
+        }
+
+        private string _TenNhanVien;
+        public string TenNhanVien
+        {
+            get { return _TenNhanVien; }
+            set { _TenNhanVien = value; OnPropertyChanged(); }
+        }
+
+        private string _HoTen;
+        public string HoTen
+        {
+            get { return _HoTen; }
+            set { _HoTen = value; OnPropertyChanged(); }
+        }
+
+        private string _ChucVu;
+        public string ChucVu
+        {
+            get { return _ChucVu; }
+            set { _ChucVu = value; OnPropertyChanged(); }
+        }
+
+        private string _HanHopDong;
+        public string HanHopDong
+        {
+            get { return _HanHopDong; }
+            set { _HanHopDong = value; OnPropertyChanged(); }
+        }
+        #endregion
+
+        #region Icommand
+        public ICommand SearchCommand { get; set; }
+        public ICommand ChuyenCV_GiahanCommand { get; set; }
+        public ICommand ThoiViecCommand { get; set; }
+        public ICommand SelectionChangedCommand { get; set; }
+        #endregion
+
+        #region Support Functions
+        public void LoadSourceComboBoxPhongBan()
+        {
+            ObservableCollection<PHONGBAN> _listPhongBan = new ObservableCollection<PHONGBAN>(DataProvider.Ins.DB.PHONGBANs);
+
+            ListPhongBan = new ObservableCollection<string>();
+            foreach (var pb in _listPhongBan)
+                ListPhongBan.Add(pb.TenPhong);
+        }
+
+        public void SearchNhanVien()
+        {   
+            ObservableCollection<NHANVIEN> _listNhanVien = new ObservableCollection<NHANVIEN>(DataProvider.Ins.DB.NHANVIENs);
+            ListThongTinNhanVien.Clear();
+
+            foreach (var nv in _listNhanVien)
+            {
+                bool validPhongBan = false;
+                bool validTen = false;
+                CHUCVU chucvu = Global.Ins.getChucVubyMaNV(nv.id);
+                if (SelectedPhongBan == null || (chucvu != null && chucvu.PHONGBAN.TenPhong == SelectedPhongBan))
+                    validPhongBan = true;
+
+                if (String.IsNullOrWhiteSpace(TenNhanVien) || nv.HoTen.Contains(TenNhanVien))
+                    validTen = true;
+
+                if (validTen && validPhongBan)
+                {
+                    int stt = _ListThongTinNhanVien.Count() + 1;
+                    ThongTinNhanVien item = new ThongTinNhanVien(stt, nv.id, nv.HoTen, chucvu.PHONGBAN.TenPhong, chucvu.TenChucVu);
+                    ListThongTinNhanVien.Add(item);
+                }
+            }
+            
+        }
+
+        public void BindingSelectionNhanVien()
+        {
+            if (SelectedNhanVien == null)
+                return;
+            HoTen = SelectedNhanVien.HoTen;
+            CHUCVU chucvu = Global.Ins.getChucVubyMaNV(SelectedNhanVien.MaNV);
+            PHONGBAN phongban = chucvu.PHONGBAN;
+            ChucVu = chucvu.TenChucVu + " - " + phongban.TenPhong;
+            NHANVIEN nv = Global.Ins.getNhanVienbyMaNV(SelectedNhanVien.MaNV);
+            HanHopDong = "";
+            HOPDONG hopdong = Global.Ins.getHopDongbyMaNV(SelectedNhanVien.MaNV);
+            if (hopdong != null)
+            { 
+
+                HanHopDong = hopdong.LOAIHOPDONG.TenLoaiHD + " - " + " Hiệu lực đến : " + hopdong.NgayKT.ToString("dd/MM/yyyy");
+
+                if (hopdong.NgayKT < DateTime.Now)
+                {
+                    HanHopDong += "  (Đã quá hạn)";
+                    ContentCommand = "Gia hạn hợp đồng";
+                }
+                else
+                    ContentCommand = "Chuyển chức vụ";
+            }
+
+            //Binding Qua trinh lam viec
+            ListQuaTrinhNhanVien.Clear();
+            List<LICHSUCHUCVU> _listLS = new List<LICHSUCHUCVU>(DataProvider.Ins.DB.LICHSUCHUCVUs);
+            foreach (var ls in _listLS)
+            {
+                if (ls.isDeleted == false && ls.MaNV == nv.id)
+                {
+                    int stt = ListQuaTrinhNhanVien.Count() + 1;
+                    string ngayBD = ls.NgayBD.ToString("dd/MM/yyyy");
+                    string ngayKT = ls.NgayKT.ToString("dd/MM/yyyy");
+                    if (ls.NgayBD.AddDays(1.1) >= ls.NgayKT)
+                        ngayKT = "Hiện tại";
+                    QuaTrinhLamViec quatrinh = new QuaTrinhLamViec(stt, ls.CHUCVU.TenChucVu, ls.CHUCVU.PHONGBAN.TenPhong, ngayBD, ngayKT);
+                    ListQuaTrinhNhanVien.Add(quatrinh);
+                }
+            }
+        }
+        
+        public void ChuyenChucVu()
+        {
+            NHANVIEN nhanvien = Global.Ins.getNhanVienbyMaNV(SelectedNhanVien.MaNV);
+            ChuyenChucVu windowChuyenChucVu = new ChuyenChucVu(nhanvien);
+            windowChuyenChucVu.ShowDialog();
+        }
+        #endregion
+
+        public QLLichSu_ViewModel()
+        {
+            #region Load Database
+            ContentCommand = "Chuyển chức vụ";
+            LoadSourceComboBoxPhongBan();
+            _ListThongTinNhanVien = new ObservableCollection<ThongTinNhanVien>();
+            _ListQuaTrinhNhanVien = new ObservableCollection<QuaTrinhLamViec>();
+            #endregion
+
+            #region Declare Icommands
+            SearchCommand = new RelayCommand<Window>((p) => { return true; }, (p) => {
+                SearchNhanVien();
+            });
+
+            SelectionChangedCommand = new RelayCommand<Window>((p) => { return true; }, (p) => {
+                BindingSelectionNhanVien();
+            });
+
+            ChuyenCV_GiahanCommand = new RelayCommand<Window>((p) => { if (SelectedNhanVien == null) return false; return true; }, (p) => {
+                if (ContentCommand == "Chuyển chức vụ")
+                {
+                    ChuyenChucVu();
+                }
+            });
+            #endregion
+        }
 
     }
 }
